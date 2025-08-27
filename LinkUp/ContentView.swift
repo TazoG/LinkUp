fb3a581cc901524d4bb32b5e6194c68fc7fd23bd
Tazo <tazogigitashvili@gmail.com>
2025-08-27 21:28:33 +0400

    Initial commit and project setup

---
+++ LinkUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LinkUp
+//
+//  Created by Tazo Gigitashvili on 27.08.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LinkUp/LinkUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkUpApp.swift
+//  LinkUp
+//
+//  Created by Tazo Gigitashvili on 27.08.25.
+//
+
+import SwiftUI
+
+@main
+struct LinkUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
